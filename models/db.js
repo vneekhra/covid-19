91c4665ad6c90f9970740e4716abd407a83a8522
vneekhra <vikasnikhra@gmail.com>
2020-06-05 13:28:30 -0600

    updated db details

--- models/db.js
+++ models/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+
+//mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => { // for locathost
+//mongoose.connect('mongodb://mongodb-server:27017/EmployeeDB', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => { //used in docker-compose file
+mongoose.connect('mongodb://root:root@mongo/containercrushdb', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => { //used in cluster
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
